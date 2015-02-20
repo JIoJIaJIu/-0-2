86054229e4bc8eaccfb79e4ad73d1246107b441b
Guro Bokum <jiojiajiu@gmail.com>
2015-02-20 20:26:37 +0700

    Notification skeleton

--- src/services/Notification.js
+++ src/services/Notification.js
@@ -1,4 +1,25 @@
 angular.module('MomAndPop.services').service('Notification', [
     '$log',
 function ($log) {
+    /**
+     * @param {Object} feedback
+     * @param {Function} callback
+     */
+    this.notifyAdminOfFeedback = function () {
+    }
+
+    /**
+     * @param {Object} reportedAbuse
+     * @param {Function} callback
+     */
+    this.notifyAdminOfReportedAbuse = function () {
+    }
+
+    /**
+     * @param {String} email
+     * @param {String} message
+     * @param {Function} callback
+     */
+    this.notifyUserOfInvitation = function () {
+    }
 }]);
