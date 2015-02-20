abd667df4a1e4bd839ab240dec93153f72b40440
Guro Bokum <jiojiajiu@gmail.com>
2015-02-20 20:22:28 +0700

    business skeleton

--- src/services/Business.js
+++ src/services/Business.js
@@ -1,4 +1,58 @@
 angular.module('MomAndPop.services').service('Business', [
     '$log',
 function ($log) {
+    /**
+     * @param {String} id
+     * @param {Function} callback
+     */
+    this.getBusiness = function () {
+    }
+
+    /**
+     * @param {Function} callback
+     */
+    this.getMyBusiness = function () {
+    }
+
+    /**
+     * @param {Object} business
+     * @param {ArrayBuffer} photo
+     * @param {Function} callback
+     */
+    this.updateMyBusiness = function () {
+    }
+
+    /** 
+     * @param {Object} card
+     * @param {Function} callback
+     */
+    this.verify = function () {
+    }
+
+    /**
+     * @param {Function} callback
+     */
+    this.getBusinessEmployees = function () {
+    }
+
+    /**
+     * @param {Object} user
+     * @param {Function} callback
+     */
+    this.addBusinessEmployee = function () {
+    }
+
+    /**
+     * @param {String} id
+     * @param {Function} callback
+     */
+    this.deleteBusinessEmployee = function () {
+    }
+
+    /**
+     * @param {Object} user
+     * @param {Function} callback
+     */
+    this.updateBusinessEmployee = function () {
+    }
 }]);
