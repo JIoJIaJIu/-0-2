d181970220e2139afa45c0b04018d25b64268a07
Guro Bokum <jiojiajiu@gmail.com>
2015-02-20 20:07:48 +0700

    Services

---
+++ src/services/GiftCard.js
@@ -0,0 +1,48 @@
+angular.module('MomAndPop.services').service('GiftCard', [
+    '$log',
+function ($log) {
+    /**
+     * @param {Object} criteria
+     * @param {Function} callback
+     */
+    this.search = function () {
+    }
+
+    /**
+     * @param {String} id
+     * @param {Function} callback
+     */
+    this.get = function () {
+    }
+
+    /**
+     * @param {String} id
+     * @param {Number} quantity
+     * @param {String} desc
+     * @param {Function} callback
+     */
+    this.resell = function () {
+    }
+
+    /**
+     * @param {String} id
+     * @param {Number} amount
+     * @param {Function} callback
+     */
+    this.prepareForRedeem = function () {
+    }
+
+    /**
+     * @param {String} qrCode
+     * @param {Number} amount
+     * @param {Function} callback
+     */
+    this.redeem = function () {
+    }
+
+    /**
+     * @param {Function} callback
+     */
+    this.getTotalRedeemedAmount = function () { 
+    }
+}]);
---
+++ src/services/Notification.js
@@ -0,0 +1,4 @@
+angular.module('MomAndPop.services').service('Notification', [
+    '$log',
+function ($log) {
+}]);
